e9599793280ccd332ba4afe856ad4856529f194d
kopervas <sergkopervas@gmail.com>
2018-07-13 18:50:23 +0300

    Варіант з пробним використанням стилю (*.css). У таблиці вже виконуються обчислення, але не зберігаються формули, і не обчислюються значення по адресах.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.beans.property.StringProperty;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -12,11 +12,14 @@ import javafx.stage.Stage;
 import java.util.function.Function;
 
 public class Main extends Application {
+    private static TableView<Row> table = new TableView<>();
+    private static ObservableList<Formula> formuls = FXCollections.observableArrayList();
+    private static char firstChar;
+    private static Formula f;
 
     @Override
     public void start(Stage primaryStage) {
-
-        TableView<Row> table = new TableView<>();
+        ObservableList<Formula> formuls = FXCollections.observableArrayList();
 
         table.getSelectionModel().setCellSelectionEnabled(true);
         table.setEditable(true);
@@ -31,8 +34,8 @@ public class Main extends Application {
         table.getColumns().add(createColumn("G", Row::colGProperty));
         table.getColumns().add(createColumn("H", Row::colHProperty));
 
-        table.getItems().addAll(
-                //new Row(" ", "", "", "", "", "", "", "", ""),
+        //table.getItems().addAll(
+        ObservableList<Row> rows = FXCollections.observableArrayList(
                 new Row("1", "", "", "", "", "", "", "", ""),
                 new Row("2", "", "", "", "", "", "", "", ""),
                 new Row("3", "", "", "", "", "", "", "", ""),
@@ -40,36 +43,99 @@ public class Main extends Application {
                 new Row("5", "", "", "", "", "", "", "", ""),
                 new Row("6", "", "", "", "", "", "", "", ""),
                 new Row("7", "", "", "", "", "", "", "", ""),
-                new Row("8", "", "", "", "", "", "", "", "")
+                new Row("8", "", "", "", "", "", "", "", ""),
+                new Row("9", "", "", "", "", "", "", "", ""),
+                new Row("10", "", "", "", "", "", "", "", "")
 
         );
+        table.setItems(rows);
 
+        /**
+         * Таблиці присвоюється обробник події - клавіатурного вводу значення в комірку
+         */
         table.setOnKeyPressed(event -> {
+            String s = " ";
             TablePosition<Row, ?> pos = table.getFocusModel().getFocusedCell();
+            //rows.get(1).getColC().replaceAll("", "replace");
+            //(rows.get(pos.getRow())).setColC("N");                            //написано лише для перевірки
             if (pos != null) {
+
+                switch (pos.getColumn()) {                                      //далі йде код отримання String з комірки
+                    case 1:
+                        s = table.getSelectionModel().getSelectedItem().getColA();      //власне так отримуємо String
+                        System.out.println(s);
+                        break;
+                    case 2:
+                        s = table.getSelectionModel().getSelectedItem().getColB();
+                        System.out.println(s);
+                        break;
+                    case 3:
+                        s = table.getSelectionModel().getSelectedItem().getColC();
+                        System.out.println(s);
+                        break;
+                    case 4:
+                        s = table.getSelectionModel().getSelectedItem().getColD();
+                        System.out.println(s);
+                        break;
+                    case 5:
+                        s = table.getSelectionModel().getSelectedItem().getColE();
+                        System.out.println(s);
+                        break;
+                    case 6:
+                        s = table.getSelectionModel().getSelectedItem().getColF();
+                        System.out.println(s);
+                        break;
+                    case 7:
+                        s = table.getSelectionModel().getSelectedItem().getColG();
+                        System.out.println(s);
+                        break;
+                    case 8:
+                        s = table.getSelectionModel().getSelectedItem().getColH();
+                        System.out.println(s);
+                        break;
+                }
+                //org.mariuszgromada.math.mxparser.Function f = new org.mariuszgromada.math.mxparser.Function();
+               /* Expression e = new Expression(s);
+                System.out.println("Result = "+e.calculate());*/    // - вся дія по обробці отриманого
+
                 table.edit(pos.getRow(), pos.getTableColumn());
+                //System.out.println(table.getSelectionModel().getSelectedItem());
+
+                System.out.println("Row index: "+pos.getRow()+", Col index: "+pos.getColumn());
+                //System.out.println((rows.get(pos.getRow())).setColC("N"));//getColC());
+                //(rows.get(pos.getRow())).setColC("N");
             }
         });
-
-        Scene scene = new Scene(new BorderPane(table), 600, 600);
+        f = new Formula();
+        Scene scene = new Scene(new BorderPane(table), 300, 275);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    /**
+     * Метод створення колонки в таблиці.
+     * @param title - назва колонки
+     * @param property - property, пов'язана зі StringProperty класу Row
+     * @return - повертається колонка
+     */
     private TableColumn<Row, String> createColumn(String title, Function<Row, StringProperty> property) {
         TableColumn<Row, String> col = new TableColumn<>(title);
         col.setCellValueFactory(cellData -> property.apply(cellData.getValue()));
 
         col.setCellFactory(column -> new EditCell(property));
-
+        col.setPrefWidth(35);
         return col;
     }
 
+    /**
+     * Клас редагування комірки. Перевизначаються методи
+     * startEdit(), cancelEdti(), commitEdit()
+     */
     private static class EditCell extends TableCell<Row, String> {
 
         private final TextField textField = new TextField();
 
-        private final Function<Row, StringProperty> property;
+        private Function<Row, StringProperty> property;
 
         EditCell(Function<Row, StringProperty> property) {
             this.property = property;
@@ -86,10 +152,19 @@ public class Main extends Application {
                     commitEdit(textField.getText());
                 }
             });
+            //На даний момент - 15.06.18, - це головна проблема. Треба додати до ObservableArray<Formula> formuls
+            //Для цього потрібно отримати текствоий рядок з комірки і значення її індексів по колонці і рядку.
+            if(isFormula(textField.getText()))
+            {
+                //TablePosition<Row, ?> pos = (textField.getClip()).get;
+                //formuls.add(new Formula(1,1,textField.getText()));
+                f.action(textField.getText());
+            }
         }
 
         @Override
         public void startEdit() {
+            super.setStyle("-fx-border-width: 0.5; -fx-border-color: #ffffff; -fx-background-color: lightskyblue");
             super.startEdit();
             textField.setText(getItem());
             setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
@@ -105,10 +180,40 @@ public class Main extends Application {
         @Override
         public void commitEdit(String text) {
             super.commitEdit(text);
-            Row row = getTableView().getItems().get(getIndex());
-            StringProperty cellProperty = property.apply(row);
-            cellProperty.set(text);
+                Row row = getTableView().getItems().get(getIndex());
+                StringProperty cellProperty = property.apply(row);
+                if(isFormula(text)) {                                   //ось цей блок коду власне виконує обчислення
+                    text = text.substring(1);                           //в комірці і повертає у неї значення
+                    /*Expression e = new Expression(text);
+                    Double d = e.calculate();*/
+                    f = new Formula(text, table);
+                    text = f.action(text);
+                    System.out.println("Result = " + text);
+                    cellProperty.set(text);
+                    super.setStyle("-fx-border-width: 0.5; -fx-border-color: #000000; -fx-background-color: #ffffff");
+            }
+            else
+                cellProperty.set(text);
+
+            super.setStyle("-fx-border-width: 0.5; -fx-border-color: #000000; -fx-background-color: #ffffff");
             setContentDisplay(ContentDisplay.TEXT_ONLY);
+        }
+
+        /**
+         * Метод, який перевіряє, чи є введений рядок формулою
+         * Повертає true, якщо перший символ у рядку - "=" і якщо
+         * в рядку є хоча б мінімально необхідна кількість символів
+         * для формули - 4.
+         * @param text
+         * @return boolean
+         */
+        private boolean isFormula(String text){
+            //firstChar = text.charAt(0);
+            //if((firstChar == '=') && (text.length() != 0)) {
+              if(text.indexOf("=") == 0 && text.length() > 3){
+                return true;
+            }
+            return false;
         }
 
         public static void main(String[] args) {
